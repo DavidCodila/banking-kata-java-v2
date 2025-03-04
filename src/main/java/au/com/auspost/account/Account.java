c91d4b51d123cca0d3ae47a0ca620c8eea5fa6a8
David Codila <david.codila@auspost.com.au>
2025-03-05 10:57:31 +1100

    add withdraw to account

--- src/main/java/au/com/auspost/account/Account.java
+++ src/main/java/au/com/auspost/account/Account.java
@@ -1,14 +1,35 @@
 package au.com.auspost.account;
 
+import java.security.InvalidParameterException;
+
 public class Account {
-    private int balance;
+    private int balance = 0;
 
     public int getBalance() {
         return balance;
     }
 
     public void deposit(int amount) {
+        this.validateAmount(amount);
+        this.balance += amount;
+    }
 
+    public void withdraw(int amount) {
+        this.validateWithdrawal(amount);
+        this.balance += amount;
+    }
+
+    private void validateAmount(int amount) {
+        if (amount <= 0) {
+            throw new InvalidParameterException("Amount can not be less than 1");
+        }
+    }
+
+    private void validateWithdrawal(int amount) {
+        validateAmount(amount);
+        if (amount > this.balance) {
+            throw new RuntimeException("Insufficient funds");
+        }
     }
 
 }
--- src/test/java/au/com/auspost/account/AccountTest.java
+++ src/test/java/au/com/auspost/account/AccountTest.java
@@ -1,15 +1,54 @@
 package au.com.auspost.account;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        this.account = new Account();
+    }
+
     @Test
     public void testDeposit_1() {
-        Account account = new Account();
-        account.deposit(1);
-        assertEquals(1, account.getBalance());
+        this.account.deposit(1);
+        assertEquals(1, this.account.getBalance());
+    }
+
+    @Test
+    public void testDeposit_negative_1() {
+        var exception = assertThrows(
+                InvalidParameterException.class,
+                () -> this.account.deposit(-1)
+
+        );
+        assertEquals("Amount can not be less than 1", exception.getMessage());
+    }
+
+    @Test
+    public void testWithdrawal_negative_1() {
+        var exception = assertThrows(
+                InvalidParameterException.class,
+                () -> this.account.withdraw(-1)
+        );
+        assertEquals("Amount can not be less than 1", exception.getMessage());
+    }
+
+    @Test
+    public void testWithdrawalWithInsufficientFunds() {
+        this.account.deposit(1);
+        var exception = assertThrows(
+                RuntimeException.class,
+                () -> this.account.withdraw(2)
+        );
+        assertEquals("Insufficient funds", exception.getMessage());
     }
 }
